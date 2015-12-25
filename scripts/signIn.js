1eccada57059a70292be8e68cc269f0b038e9fd5
Ian Shinbrot <ianshinbro@gmail.com>
2015-12-25 16:05:07 -0500

    fixing modal width size. still gotta fix the height

---
+++ scripts/signIn.js
@@ -0,0 +1,15 @@
+
+app.controller('signInPage', function () {
+
+});
+
+
+
+function resetPassword() {
+    window.location.href="resetPassword.html";
+}
+
+function rememberMe() {
+    //TODO: implement remember me functionality
+
+}
