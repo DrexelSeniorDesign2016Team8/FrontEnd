a86caa4a56b8358753a85c838f8397be42446366
Ian Shinbrot <ianshinbro@gmail.com>
2015-12-28 12:00:02 -0500

    reset Password now shows  confirmation message upon click. Still working on the message for the dialog box

--- scripts/resetPassword.js
+++ scripts/resetPassword.js
@@ -4,14 +4,12 @@
 
 
 
-app.controller('resetPasswordController', function ($scope, $timeout, $mdSidenav, $log) {
-    app.controller('resetPassword', function ($scope, $timeout, $mdSidenav, $log) {
-        $scope.showConfirmation = function () {
-            return $mdSidenav('searchBar').isOpen();
-
+app.controller('resetPasswordController', function ($scope, $timeout, $log) {
+    $scope.showConfirmation = false;
+        $scope.sendEmailResetPassword = function () {
+           // sendEmail($scope.resetPassword.emailAddress);
+            $scope.showConfirmation = true;
         };
-        ;
-    })
 });
 
 
@@ -19,20 +17,6 @@ function sendEmail(emailAddress) {
     //TODO Send API call to send a password to an email address
     alert("This to be implemented in the future for" + emailAddress);
 }
-function disable(element) {
-    // enable/disable buttons for added fun :)
-    element.addClass("mdl-button--disabled");
-
-}
-function enable(element) {
-    // enable/disable buttons for added fun :)
-    if (element.hasClass("mdl-button--disabled")) {
-        element.removeClass("mdl-button--disabled");
-    }
-}
 
 
 
-function sendEmailResetPassword() {
-
-}
--- scripts/signIn.js
+++ scripts/signIn.js
@@ -7,7 +7,26 @@ function signInController($scope, $mdDialog, $log) {
         user: ''
     };
 
+    $scope.createAccount = function() {
+        $scope.login.loading = true;
 
+
+        // TODO: login call service with a callback
+
+        var success=true;
+        if (success) {
+            $log.debug('login successful')
+
+            //TODO adjust page so logged in information is now shown
+            createCookie("loggedIn", "false");
+            $scope.currentUserLoggedin = true;
+            $mdDialog.hide();
+
+        }
+        else {
+            $log.debug('login failed');
+        }
+    }
     $scope.signIn = function () {
         $scope.login.loading = true;
 
@@ -39,6 +58,20 @@ function signInController($scope, $mdDialog, $log) {
     };
 }
 
+app.directive('pwCheck', [function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            var firstPassword = '#' + attrs.pwCheck;
+            elem.add(firstPassword).on('keyup', function () {
+                scope.$apply(function () {
+                    var v = elem.val() === $(firstPassword).val();
+                    ctrl.$setValidity('pwmatch', v);
+                });
+            });
+        }
+    }
+}]);
 function resetPassword() {
     window.location.href="resetPassword.html";
 }
