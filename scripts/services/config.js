8e37c19735dee1a41323e9cf422c75d9b3758e28
ishinbrot <ianshinbro@gmail.com>
2016-01-20 22:44:29 -0500

    using a directive for the search results to allow more flexibilty in the future

--- scripts/services/config.js
+++ scripts/services/config.js
@@ -34,3 +34,26 @@ app.directive('showFocus', function($timeout) {
         }
         
     })
+
+app.directive('searchResults', function() {
+    return {
+        template: "<h2> {{college.name }}</h2>" +
+        "<div ng-if='college.image!=undefined'><img src='{{college.image}}.jpg' alt='{{college.name}}'' style='width:200px;height:200px; float:left'></div>" +
+        "<div ng-f='college.image==undefined'><label class='collegeLabel' collegeLetter></div>" +
+        "<div ng-if='college.address!==undefined'><p>{{college.address}}</p></div>" +
+        "<div ng-if='college.address==undefined'> <p>Address unavailable</p></div>" +
+        "<div ng-if='college.website!=undefined && college.phoneNumber==undefined'> {college.website}} Number Unavailable</div>" +
+        "<div ng-if='college.website==undefined && college.phoneNumber==undefined'>Website unavailable Number Unavailable</div>" +
+        "<div ng-if='college.website==undefined && college.phoneNumber!=undefined'>Website unavailable {{college.phoneNumber}}</div>" +
+        "<div ng-if='college.phoneNumber!=undefined && college.website!=undefined'> {{college.website}} {{college.phoneNumber}}</div>" +
+        "<div ng-if='college.phoneNumber==undefined'><p>Number unavailable</p></div>" +
+        "<div ng-if='college.population!==undefined'><p>Students: {{college.population}}</p></div>" +
+        "<div ng-if='college.population==undefined'><p>Student count unavailable</p></div>" +
+        "<hr>",
+
+        link: function (scope, element, attrs) {
+            scope.image = attrs.image
+
+        }
+    }
+})
