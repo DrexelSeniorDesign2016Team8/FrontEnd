032c460531f1261fe80986f403c36d9c6c387327
ishinbrot <ianshinbro@gmail.com>
2016-02-18 19:23:21 -0500

    reformatting userService create account parameters

--- scripts/services/userService.js
+++ scripts/services/userService.js
@@ -125,8 +125,8 @@ app.factory('userService', function($localStorage) {
     function generateCreateAccountParameters(userInfo) {
         var parameters ='';
         parameters += "name=" + userInfo.name + "&";
-        parameters += "email" + userInfo.userName +"&";
-        parameters += "pass" + userInfo.password + "&";
+        parameters += "email=" + userInfo.userName +"&";
+        parameters += "pass=" + userInfo.password + "&";
         return parameters;
     }
     function setLoginStatus(status) {
