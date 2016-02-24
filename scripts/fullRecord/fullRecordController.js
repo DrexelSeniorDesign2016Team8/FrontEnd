7b13cfb1f6d3e9f4af270949bf322ae2e5116712
ishinbrot <ianshinbro@gmail.com>
2016-02-23 20:23:46 -0500

    fixing full record page

--- scripts/fullRecord/fullRecordController.js
+++ scripts/fullRecord/fullRecordController.js
@@ -1,6 +1,9 @@
 
 function fullRecordController ($scope, $mdDialog, $mdMedia,userService, navigationService) {
 
+    $scope.items=items;
+
+    college=$scope.items;
 
 };/**
  * Created by ianshinbro on 2/23/2016.
