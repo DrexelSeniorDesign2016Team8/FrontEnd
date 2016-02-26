afe8242abbfb516c531724959c78ba42a556d2bc
ishinbrot <ianshinbro@gmail.com>
2016-02-26 18:53:47 -0500

    full record page should now close

--- scripts/fullRecord/fullRecordController.js
+++ scripts/fullRecord/fullRecordController.js
@@ -1,10 +1,18 @@
 
-function fullRecordController ($scope, items, $mdDialog, $mdMedia,userService, navigationService) {
+function fullRecordController ($scope, items, $mdDialog, $mdMedia) {
 
     $scope.items=items;
 
     $scope.college=$scope.items;
 
+
+
+$scope.hide = function() {
+    $mdDialog.hide();
+};
+$scope.cancel = function() {
+    $mdDialog.cancel();
+};
 };/**
  * Created by ianshinbro on 2/23/2016.
  */
