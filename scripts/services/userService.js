c5427f51ebd30b92d2c746cfb423c821bb118215
ishinbrot <ianshinbro@gmail.com>
2016-03-01 22:43:01 -0500

    fixing full record page

--- scripts/services/userService.js
+++ scripts/services/userService.js
@@ -100,9 +100,9 @@ app.factory('userService', function($localStorage, searchService, apiCall) {
      */
     function restoreLocalStorage() {
         if ($localStorage.username && $localStorage.loggedIn) {
-            user.username = setUserName($localStorage.username);
-            user.loggedIn = setLoggedIn($localStorage.loggedIn);
-            user.sessionId=setSessionId($localStorage.sessionId);
+            setUserName($localStorage.username);
+            setLoggedIn($localStorage.loggedIn);
+            setSessionId($localStorage.sessionId);
             if ($localStorage.rememberMe==false && $localStorage.username=="") {      // if remember me is not selected remove local storage
                 $localStorage.loggedIn=false;
                 $localStorage.username='';
