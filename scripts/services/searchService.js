7b0d9c4d8336aebfb5756b6f5aa4ee7db1fae45a
ishinbrot <ianshinbro@gmail.com>
2016-03-06 01:41:16 -0500

    fixing state drop down

--- scripts/services/searchService.js
+++ scripts/services/searchService.js
@@ -89,12 +89,12 @@ app.factory('searchService', function($localStorage, apiCall) {
     }
 
     function fillStates() {
-        return ('None,' + 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,' +
-            'Florida Georgia, Hawaii, Idaho, Illinois Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland,' +
-            'Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire,' +
-            'New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania,' +
-            'Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington,' +
-            'West Virginia, Wisconsin, Wyoming,'
+        return ('None,' + 'Alabama,Alaska,Arizona,Arkansas,California,Colorado,Connecticut,Delaware,' +
+            'Florida Georgia,Hawaii,Idaho,Illinois,Indiana,Iowa,Kansas,Kentucky,Louisiana,Maine,Maryland,' +
+            'Massachusetts,Michigan,Minnesota,Mississippi,Missouri,Montana,Nebraska,Nevada,New Hampshire,' +
+            'New Jersey,New Mexico,New York,North Carolina,North Dakota,Ohio,Oklahoma,Oregon,Pennsylvania,' +
+            'Rhode Island,South Carolina,South Dakota,Tennessee,Texas,Utah,Vermont,Virginia,Washington,' +
+            'West Virginia,Wisconsin,Wyoming,'
         ).split(',').map(function (state) {
             return {fullName: state};
         });
