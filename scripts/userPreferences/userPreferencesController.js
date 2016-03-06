f0dd15bcb3e87929ff92efb30d8c001dc8ee1692
ishinbrot <ianshinbro@gmail.com>
2016-03-06 01:24:20 -0500

    states are now populated at the end

--- scripts/userPreferences/userPreferencesController.js
+++ scripts/userPreferences/userPreferencesController.js
@@ -153,8 +153,9 @@ app.controller('userPreferencesController', function ($scope, $mdDialog, $log, u
             if (response[0].ACTScore) {
                 response[0].actcomposite = parseInt(response[0].ACTScore);
             }
-            $scope.parameter.states = searchService.fillStates();
+
             $scope.parameter=response[0];
+            $scope.parameter.states = searchService.fillStates();
             searchService.set(response[0])
         });
 
