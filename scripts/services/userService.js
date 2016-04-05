ecb83ec1c0e739c8fac82936d13ef42fd04c5d6e
ianshinbro <ianshinbro@gmail.com>
2016-04-05 17:45:43 -0400

    refactoring user service

--- scripts/services/userService.js
+++ scripts/services/userService.js
@@ -71,7 +71,7 @@ app.factory('userService', function($localStorage, searchService, apiCall) {
                 }
                 if (response[0].stateName) {
                     // convert state name
-                    convert_state(response[0].stateName, "abbrev")
+                    convert_state(response[0].stateName, "name")
                 }
             }
 
