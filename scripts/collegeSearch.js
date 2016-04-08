e06e8036a11d166a53fe8c79de18b286a2f02c80
ianshinbro <ianshinbro@gmail.com>
2016-04-08 12:14:40 -0400

    removing ui router dependency

--- scripts/collegeSearch.js
+++ scripts/collegeSearch.js
@@ -1,6 +1,6 @@
 //var loggedIn = false;
 
-var app = angular.module( 'CollegeSearch', [ 'ngMaterial', 'ngMessages', 'ngAnimate','ngAria','ngStorage', 'ui.router', 'angularUtils.directives.dirPagination']);		// initialize the app for all classes
+var app = angular.module( 'CollegeSearch', [ 'ngMaterial', 'ngMessages', 'ngAnimate','ngAria','ngStorage', 'angularUtils.directives.dirPagination']);		// initialize the app for all classes
 
 
 	app.run(function ($rootScope, $state, userService, navigationService, searchService, apiCall, $localStorage) {
