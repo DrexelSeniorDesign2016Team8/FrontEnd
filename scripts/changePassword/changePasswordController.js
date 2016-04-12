681a47dfd9c184709fba13844ca6350d1412e571
ianshinbro <ianshinbro@gmail.com>
2016-04-11 21:36:36 -0400

    adding change password page, and look of reset password page

--- scripts/changePassword/changePasswordController.js
+++ scripts/changePassword/changePasswordController.js
@@ -1,25 +1,26 @@
-app.controller('resetPasswordController', function ($scope, authService, $timeout, $log, $mdToast) {
+/**
+ * Ian Shinbrot
+ */
+app.controller('changePasswordController', function ($scope, authService, $timeout, $log, $mdToast) {
     $scope.showConfirmation = false;
     $scope.page = {};
+    $scope.page.Title="Change Password";
+    $scope.page.Message="Enter a password below";
+    $scope.sendEmailResetPassword = function () {
 
-    $scope.page.Title = "Reset Password";
-    $scope.page.Message = "Enter Your email Address below to recover your password";
-        $scope.sendEmailResetPassword = function () {
-            var emailAddress = $scope.resetPasswordform.emailAddress.$viewValue;
-
-            authService.resetPassword(emailAddress, function(response) {
+        authService.changePassword(emailAddress, function(response) {
                 // success
-                var message = "Recovery Email Sent";
+                var message = "Rocovery Email Sent";
                 showMessage(message);
             },
             function(response) {
                 //failure at retrieval
 
             });
-           var message = 'Something went wrong';
-                showMessage(message);
+        var message = 'Something went wrong';
+        showMessage(message);
 
-        }
+    }
 
     showMessage = function(message) {
 
