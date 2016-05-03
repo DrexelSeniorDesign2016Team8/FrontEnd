d1f80fd8d7693b5ca055df5c4717d1a2c67cc094
ianshinbro <ianshinbro@gmail.com>
2016-05-03 19:19:37 -0400

    user name should now be created

--- scripts/services/userService.js
+++ scripts/services/userService.js
@@ -171,7 +171,7 @@ app.factory('userService', function($localStorage, searchService, apiCall) {
     function getUserNameCall() {
         apiCall.setApiDestination("getUser.php?");
         apiCall.callCollegeSearchAPI(function(response) {
-            user.username=response.userName;
+            user.username=response.response.userName;
         });
     }
 
